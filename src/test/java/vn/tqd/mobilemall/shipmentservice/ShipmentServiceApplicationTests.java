68a7c810b81babe72028d67c58eb195ce33126ce
zamcha <caotran293@gmail.com>
2025-11-27 12:56:59 +0700

    first commit

---
+++ src/test/java/vn/tqd/mobilemall/shipmentservice/ShipmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.tqd.mobilemall.shipmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
